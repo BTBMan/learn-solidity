3146df2205ccc97b9271c883fa4b9f3f75a5bbcd
BTBMan <johnwon.me@gmail.com>
2024-07-01 23:53:41 +0800

    chore: delete

--- Array.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-contract Array {
-    // 通过 类型[] 来定义数组类型
-    // 类型方括号里可以填数字用来表示固定长度的数组 uint256[3]
-    uint256[] public number = [1,2,3];
-}

288fbf335ff8404968bc2211232dd8c64da21059
BTBMan <1498118157@qq.com>
2024-07-02 23:42:06 +0800

    feat: ERC20
    Signed-off-by: BTBMan <1498118157@qq.com>

--- ERC20.sol
+++ ERC20.sol
@@ -6,9 +6,11 @@ pragma solidity ^0.8.4;
 
 import "./IERC20.sol";
 
-abstract contract ERC20 is IERC20 {
+contract ERC20 is IERC20 {
   // 通过 override 修饰 public 变量, 会重写父合约中与当前变量同名的 getter 函数, 这里重写的是 balanceOf 和 allowance
+  // 用来存储每个地址对应的代币数量
   mapping(address => uint256) public override balanceOf;
+  // 用来存储授权者给被授权者的代币数量
   mapping(address => mapping(address => uint256)) public override allowance;
 
   // 代币总供给
@@ -37,5 +39,48 @@ abstract contract ERC20 is IERC20 {
     emit Transfer(msg.sender, to, amount); // 符合触发 Transfer 事件的条件
 
     return true;
+  }
+
+  // 实现授权函数, 被授权者可以支配授权者 amount 数量的代币
+  function approve(
+    address spender,
+    uint256 amount
+  ) external override returns (bool) {
+    allowance[msg.sender][spender] = amount;
+
+    emit Approval(msg.sender, spender, amount); // 符合触发 Approvel 事件的条件
+
+    return true;
+  }
+
+  // 实现 transferFrom 函数, 从授权方转一定数量的代币给接收者
+  function transferFrom(
+    address from,
+    address to,
+    uint256 amount
+  ) external override returns (bool) {
+    allowance[from][msg.sender] -= amount; // 授权方减去一定数量的代币
+    balanceOf[from] -= amount; // 发送方减去一定数量余额
+    balanceOf[to] += amount; // 接收者增加一定数量的代币
+
+    emit Transfer(from, to, amount); // 符合触发 Transfer 事件的条件
+
+    return true;
+  }
+
+  // 铸币函数, 这里任何人都可以铸币, 实际当中是须要加权限的, 只有 owner 可以铸币
+  function mint(uint256 amount) external {
+    balanceOf[msg.sender] += amount;
+    totalSupply += amount;
+
+    emit Transfer(address(0), msg.sender, amount); // 符合触发 Transfer 事件的条件, 这里的 from 应该是个 0x00 的地址
+  }
+
+  // 销毁代币函数
+  function burn(uint256 amount) external {
+    balanceOf[msg.sender] -= amount;
+    totalSupply -= amount;
+
+    emit Transfer(msg.sender, address(0), amount); // 符合触发 Transfer 事件的条件, 这里的 to 应该是 0x00 的地址, 调用者销毁了 0 地址一定数量的代币
   }
 }

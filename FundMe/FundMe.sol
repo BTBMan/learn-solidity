f785885d292e8130e1df79e51a5e1a043d942055
BTBMan <1498118157@qq.com>
2024-07-15 23:10:05 +0800

    feat: saving gas and safe payment
    Signed-off-by: BTBMan <1498118157@qq.com>

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -3,27 +3,38 @@ pragma solidity ^0.8.4;
 
 import "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
   using PriceConverter for uint256;
 
-  address public owner;
+  // use constant to save gas fee
+  uint256 public constant MINIMUM_USD = 5e18;
 
-  uint256 public minimumUsd = 5e18;
+  // use immutable to save gas fee
+  address public immutable i_owner;
 
   address[] public funders;
   mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
   constructor() {
-    owner = msg.sender; // save deployer
+    i_owner = msg.sender; // save deployer
   }
 
   modifier onlyOwner() {
-    require(msg.sender == owner, "Sender is not owner!");
+    // require(msg.sender == i_owner, "Sender is not owner!");
+    // do this way is gonna save gas fee
+    if (msg.sender != i_owner) {
+      revert NotOwner();
+    }
     _;
   }
 
   function fund() public payable {
-    require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!");
+    require(
+      msg.value.getConversionRate() >= MINIMUM_USD,
+      "Didn't send enough!"
+    );
 
     funders.push(msg.sender);
     addressToAmountFunded[msg.sender] += msg.value;
@@ -41,5 +52,13 @@ contract FundMe {
     }("");
 
     require(callSuccess, "Call failed!");
+  }
+
+  receive() external payable {
+    fund();
+  }
+
+  fallback() external payable {
+    fund();
   }
 }
